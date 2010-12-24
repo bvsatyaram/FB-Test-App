eea8d5bb83a76113d44821044f51c711483376db
B V Satyaram <bvsatyaram@gmail.com>
2010-12-24 11:43:50 +0530

    Stream Publish

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module FbTestApp
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+     config.action_view.javascript_expansions[:defaults] = ['http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.7/jquery-ui.min.js', 'jquery-ujs', 'http://connect.facebook.net/en_US/all.js']
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,51 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(function() {
+  FB.init({
+    appId  : '127104050685557',
+    status : true, // check login status
+    cookie : true, // enable cookies to allow the server to access the session
+    xfbml  : true  // parse XFBML
+  });
+});
+
+
+function publishResponse(response)
+{
+  if (response && response.post_id) {
+    alert('Post was published.');
+    // generateFlash(response.session);
+  } else {
+    alert('Post was not published.');
+  }
+}
+
+function publishToWall()
+{
+  publish = {
+    method: 'feed',
+    message: 'Its great!!',
+    name: 'Chronus Test App',
+    caption: 'The new Facebook Graph API',
+    description: (
+      'At Facebook\'s core is the social graph; people and the connections they have ' +
+      'to everything they care about. The Graph API presents a simple, consistent view ' +
+      'of the Facebook social graph, uniformly representing objects in the graph ' +
+      '(e.g., people, photos, events, and pages) and the connections between them ' +
+      '(e.g., friend relationships, shared content, and photo tags).'
+      ),
+    link: 'http://chronus.com/',
+    picture: 'http://chronus.com/images/chronus-logo.png',
+    actions: [
+    {
+      name: 'chronus',
+      link: 'http://chronus.com/'
+    }
+    ],
+    display: 'popup',
+    session_key: FB.getSession()
+  };
+  
+  FB.ui(publish, publishResponse);
+}
+
